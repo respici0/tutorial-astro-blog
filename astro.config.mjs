1d9c290e7aa2675345b40d1731dda299d4091244
Robert Respicio <respicio.dev+github@gmail.com>
2025-12-05 08:07:44 -0800

    finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://fc07d3e2.tutorial-astro-blog.pages.dev/",
+  integrations: [react()],
+});
